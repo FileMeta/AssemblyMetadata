816be7a614b158aabb6a90053e144b7d14a2ff2e
Brandt Redd <brandt@redd.org>
2021-10-26 19:33:45 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using FileMeta;
+using System.Diagnostics;
+
+namespace UnitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var metadata = new AssemblyMetadata(typeof(Program));
+            Console.WriteLine(metadata.Summary);
+        }
+
+    }
+}
